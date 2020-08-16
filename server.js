03877522e82e785f6792fb5f8eb02eb19207b724
Nathan Hall <Nashing1@gmail.com>
2020-08-15 22:48:17 -0500

    html routes working
    html routes now display when you load localhost

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const routes = require('./routes/api');
+const api = require('./routes/api');
 const html = require('./routes/html');
 
 const app = express();
@@ -12,6 +12,7 @@ const PORT = 9000;
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(express.static('public'))
+html(app);
 
 app.listen(PORT, () => console.log(`API is listening on port ${PORT}`));
